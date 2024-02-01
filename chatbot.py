d58f4bfd90d75d3a678950f44eef499812fe738c
Taha Merghani <taha.y.merghani@gmail.com>
2024-02-01 15:22:30 +0000

    moved to fully local with ollama

--- chatbot.py
+++ chatbot.py
@@ -1,40 +1,20 @@
-import openai
 import os
 import whisper
 import sys
 import subprocess
-def get_api_key():
-    return os.environ["OPENAI_API_KEY"]
-def speech2text(audio_path, model="whisper-1", api_key=get_api_key(), locally=False):
+
+def speech2text(audio_path):
   '''
   Function to transcribe an audio file to text using OpenAI Speech API or locally 
   '''
-  if locally:
-    print('locally ...')
-    return whisper.load_model("base").transcribe(audio_path)["text"]
-  client = openai.OpenAI(api_key=api_key)
-  audio_file = open(audio_path, "rb")
-  transcript = client.audio.transcriptions.create( 
-    model=model,
-    file=audio_file
-  )
-  return transcript.text
-def prompt2answer(prompt, model="gpt-4", api_key=get_api_key(), locally=False):
+  return whisper.load_model("base").transcribe(audio_path)["text"]
+def prompt2answer(prompt):
   """ 
   Function to answer question by autocompletion 
   """
-  if locally:
-    print('locally ...')
-    result = subprocess.run([f"""
-    llama.cpp/main 2>/dev/null -e -p "{prompt}" -m "../models/mistral-7b-instruct-v0.1.Q6_K.gguf" -ngl 30000"""], shell=True,  stdout=subprocess.PIPE, text=True)
-    return result.stdout
-  client = openai.OpenAI(api_key=api_key)
-  completion = client.chat.completions.create(
-    model=model,messages=
-    [ {"role": "system", "content": prompt} ]
-  )
-  return completion.choices[0].message.content
-
+  result = subprocess.run([f"""
+  ollama run mistral "{prompt}" """], shell=True,  stdout=subprocess.PIPE, text=True)
+  return result.stdout
 
 def main():
     if len(sys.argv) < 2:
@@ -43,10 +23,10 @@ def main():
 
     print("Chatbot started")
     audio_path = sys.argv[1]
-    transcript = speech2text(audio_path, locally=True)
+    transcript = speech2text(audio_path)
     print(f'Transcript: {transcript}')
     prompt = transcript
-    response = prompt2answer(prompt, locally=True)
+    response = prompt2answer(prompt)
     print(f"Chatbot: {response}")
 
 if __name__ == '__main__':
